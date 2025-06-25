9d141c050df03e99969073cad8573f1b02b6828b
Martina Garcia <mgarcia.06@outlook.es>
2025-06-25 12:23:03 -0300

    add ejercicio 2

---
+++ ejercicio-2/script.js
@@ -0,0 +1,24 @@
+const formulario = document.getElementById("formulario")
+const palabra = document.getElementById("palabra")
+const ul = document.getElementById("ul-lista")
+const filtrar = document.getElementById("filtrar") 
+const animales = ["Cuervo", "Liebre", "Caracol", "Mapache", "Rana", "Ciervo", "Mariposa", "Jaguar", "Jirafa", "Lombriz", "Cisne", "Foca", "Oso", "Rinoceronte", "Tortuga", "Pulpo", "Panda", "Nutria", "Perro", "Gato", "Araña", "Ardilla"]
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault()
+    })
+
+const filtrado = () => {
+    const texto = palabra.value.toLowerCase()
+    ul.innerHTML = ""
+
+    const filtrados = animales.filter(animal => animal.toLowerCase().includes(texto))
+    filtrados.forEach(animal => {
+        const li = document.createElement("li")
+        li.textContent = animal
+        ul.appendChild(li)
+    })
+}
+
+//palabra.addEventListener("input", filtrado)
+filtrar.addEventListener("click", filtrado)
